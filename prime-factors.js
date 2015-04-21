e4fc2fad904fe2cb2c52af23a482179c1a41c5e1
Steve Hynding <steve.hynding@gmail.com>
2015-04-21 11:54:03 -0700

    2nd test - added logic to pass

--- prime-factors.js
+++ prime-factors.js
@@ -1,6 +1,15 @@
 var PrimeFactors = {
 	generate: function(nInteger) {
-		return [];
+		var factors = [], currentFactor = Math.floor(nInteger/2);
+		
+		while (currentFactor > 1) {
+			if (nInteger % currentFactor === 0) {
+				factors.push(currentFactor);
+			}
+			currentFactor--;
+		}
+		
+		return factors;
 	}
 };
 
